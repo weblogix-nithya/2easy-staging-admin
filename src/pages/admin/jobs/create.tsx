3df33bc9631fb088540d6631c3349e9cf37e8a13
sangeethaRamkumar <sangeetha.m@weblogix.com.au>
2025-05-20 16:45:44 +0530

    transport type require removed

--- src/pages/admin/jobs/create.tsx
+++ src/pages/admin/jobs/create.tsx
@@ -979,16 +979,16 @@ function JobEdit() {
       return;
     }
 
-    if (refinedData.transport_type === null || "") {
-      toast({
-        title: "transport Type Required",
-        description: "Please select Import or Export as the transport type.",
-        status: "warning",
-        duration: 3000,
-        isClosable: true,
-      });
-      return;
-    }
+    // if (refinedData.transport_type === null || "") {
+    //   toast({
+    //     title: "transport Type Required",
+    //     description: "Please select Import or Export as the transport type.",
+    //     status: "warning",
+    //     duration: 3000,
+    //     isClosable: true,
+    //   });
+    //   return;
+    // }
 
     const today = new Date().toISOString(); // Gets current date and time in ISO format
 
