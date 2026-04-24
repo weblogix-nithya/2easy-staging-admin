fb17060112d884f33951847b7acbf68a145b8e44
sangeetha M <sangeetha.m@weblogix.com.au>
2026-04-24 17:49:55 +0530

    driver check -test 3

--- src/pages/admin/jobs/drivercheck/DriverPath.tsx
+++ src/pages/admin/jobs/drivercheck/DriverPath.tsx
@@ -48,6 +48,7 @@ export default function DriverPathPage() {
         page: 1,
       },
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <DriverPathMap points={points} />;
